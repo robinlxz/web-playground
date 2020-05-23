5dc39211e4678a24a9d8e316aea3c65110b28e0b
Xuzheng <linxz@garena.com>
2020-05-23 11:49:33 +0800

    UPD: use vuex for BlogShow

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,29 @@
-import Vue from "vue";
-import Vuex from "vuex";
+import Vue from 'vue'
+import Vuex from 'vuex'
+import BlogService from '@/services/BlogService.js'
 
-Vue.use(Vuex);
+Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
+  state: {
+    blogObject: {}
+  },
+  mutations: {
+    SET_BLOG_OBJ(state, blog) {
+      state.blogObject = blog
+    }
+  },
+  actions: {
+    fetchOneBlog(context, { id }) {
+      console.log(id)
+      BlogService.getBlogById(id)
+        .then(response => {
+          context.commit('SET_BLOG_OBJ', response.data)
+        })
+        .catch(error =>
+          console.log('Error during calling getBlogById API:' + error)
+        )
+    }
+  },
   modules: {}
-});
+})
--- src/views/Blog/BlowShow.vue
+++ src/views/Blog/BlowShow.vue
@@ -1,43 +1,37 @@
 <template>
   <div class="show">
     <h1>{{ blogObject.title }}</h1>
-    <!-- <pre><p>{{ blogObject.content }}</p></pre>
-    <br />
-    <h1>Below is formatted</h1>-->
     <pre><p v-if="blogObject.content">{{ formattedContent }}</p></pre>
   </div>
 </template>
 
 <script>
-import BlogService from '@/services/BlogService.js'
+import { mapState } from 'vuex'
 
 export default {
   name: 'BlogShow',
   data() {
     return {
-      blogObject: {}
+      // blogObject: {}
     }
   },
-  // props: {
-  //   id: String
-  // },
-  props: ['id'],
+  props: {
+    id: {
+      default: 1,
+      validator: i => parseInt(i) > 0
+    }
+  },
+  // props: ['id'],
   created() {
-    BlogService.getBlogById(this.id)
-      .then(response => {
-        this.blogObject = response.data
-      })
-      .catch(error =>
-        console.log('Error during calling getBlogById API:' + error)
-      )
+    this.$store.dispatch('fetchOneBlog', { id: this.id })
   },
   computed: {
+    ...mapState(['blogObject']),
     formattedContent() {
       return this.blogObject.content
         .split('')
         .map(char => {
           if ((char == '，') | (char == '。') | (char == ',') | (char == '.')) {
-            // if (char in ['，', '。', ',', '.']) {
             return char + '\n'
           } else {
             return char
