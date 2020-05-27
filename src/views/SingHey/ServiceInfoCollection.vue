54175ab0e779fa7e6c93981453357c09d228f831
Xuzheng <linxz@garena.com>
2020-05-27 16:26:02 +0800

    UPD: move service-choice into component, and remove headlines inside first

---
+++ src/views/SingHey/ServiceInfoCollection.vue
@@ -0,0 +1,106 @@
+<template>
+  <div class="service-info-collection">
+    <div class="service-choice">
+      <div class="tab" :class="{ active: serviceActive == 'pickup'}" @click="activatePickup">PICK-UP</div>
+      <div
+        class="tab"
+        :class="{ active: serviceActive == 'delivery'}"
+        @click="activateDelivery"
+      >DELIVERY</div>
+    </div>
+    <div class="service-info">
+      <div :class="{'display-none': serviceActive != 'pickup'}">
+        <!-- <h4>pickup-info</h4> -->
+        <datepicker v-model="pickup.date" placeholder="Select a date" />
+        <!-- <h5>Other pickup needed information to be added</h5> -->
+        <!-- <label>Select a time</label>
+        <select v-model="pickup.time">
+          <option v-for="time in times" :key="time">{{ time }}</option>
+        </select>
+        <h2>Where is my option?</h2>-->
+      </div>
+      <div :class="{'display-none': serviceActive != 'delivery'}">
+        <!-- <h4>delivery-info</h4> -->
+        <form>
+          <input type="text" placeholder="postcode for delivery" />
+        </form>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Datepicker from 'vuejs-datepicker'
+
+export default {
+  name: 'ServiceInfoCollection',
+  components: {
+    Datepicker
+  },
+  data() {
+    const times = []
+    for (let i = 1; i <= 24; i++) {
+      times.push(i + ':00')
+    }
+    return {
+      times,
+      serviceActive: 'pickup',
+      pickup: {
+        date: '',
+        time: ''
+      }
+    }
+  },
+  methods: {
+    activatePickup() {
+      this.serviceActive = 'pickup'
+    },
+    activateDelivery() {
+      this.serviceActive = 'delivery'
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '@/styles/SingHey/singheymain.scss';
+
+.service-info-collection {
+  // margin: 0 auto;
+
+  .service-choice {
+    // height: 2rem;
+    color: #fff;
+    // width: 100%;
+    display: flex;
+    margin: 0.5rem 0.5rem 0 0.5rem;
+
+    .tab {
+      background-color: $secondary-color;
+      width: 50%;
+      text-align: center;
+      padding: 1rem 0 1rem 0;
+      border-radius: 20px 20px 0 0;
+    }
+
+    .active {
+      background-color: $primary-color;
+    }
+  }
+
+  .service-info {
+    height: 10rem;
+    background-color: $primary-color;
+    margin: 0 0.5rem 0.5rem 0.5rem;
+    border-radius: 0 0 20px 20px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+
+    .display-none {
+      display: none;
+    }
+  }
+}
+</style>
--- src/views/SingHey/SingHomepage.vue
+++ src/views/SingHey/SingHomepage.vue
@@ -1,46 +1,27 @@
 <template>
   <div class="homepage">
-    <nav>
+    <!-- <nav>
       <i class="fas fa-bars"></i>
       <div class="title">SING HEY</div>
       <i class="fas fa-shopping-cart"></i>
-    </nav>
-    <div class="service-choice">
-      <div class="tab" :class="{ active: serviceActive == 'pickup'}" @click="activatePickup">PICK-UP</div>
-      <div
-        class="tab"
-        :class="{ active: serviceActive == 'delivery'}"
-        @click="activateDelivery"
-      >DELIVERY</div>
+    </nav>-->
+    <div class="nav">
+      <i class="fas fa-bars"></i>
+      <div class="title">SING HEY</div>
+      <i class="fas fa-shopping-cart"></i>
     </div>
-    <div class="service-info">
-      <div :class="{'display-none': serviceActive != 'pickup'}">
-        <h4>pickup-info</h4>
-        <datepicker v-model="pickup.date" placeholder="Select a date" />
-        <h5>Other pickup needed information to be added</h5>
-        <!-- <label>Select a time</label>
-        <select v-model="pickup.time">
-          <option v-for="time in times" :key="time">{{ time }}</option>
-        </select>
-        <h2>Where is my option?</h2>-->
-      </div>
-      <div :class="{'display-none': serviceActive != 'delivery'}">
-        <h4>delivery-info</h4>
-        <form>
-          <input type="text" placeholder="postcode for delivery" />
-        </form>
-      </div>
-    </div>
+    <ServiceInfoCollection />
     <div class="banner">
-      <p>SingHey focus on cellphone version first</p>
-      <h4>Banner with sliding pictures</h4>
+      <!-- <p>SingHey focus on cellphone version first</p> -->
+      <!-- <h4>Banner with sliding pictures</h4> -->
     </div>
     <br />
     <div class="content">
       <div class="singhey-menu">
+        <!-- container valign-wrapper -->
         <p>Here to add components for menu</p>
         <div class="row">
-          <div class="col s12 m6">
+          <div class="col s12">
             <div class="card">
               <div class="card-image">
                 <img src="@/assets/singhey/food1.jpg" />
@@ -56,7 +37,7 @@
           </div>
         </div>
         <div class="row">
-          <div class="col s12 m6">
+          <div class="col s12">
             <div class="card">
               <div class="card-image">
                 <img src="@/assets/singhey/food2.jpg" />
@@ -77,34 +58,35 @@
 </template>
 
 <script>
-import Datepicker from 'vuejs-datepicker'
+import ServiceInfoCollection from './ServiceInfoCollection.vue'
 
 export default {
   name: 'SingHomepage',
   components: {
-    Datepicker
+    // Datepicker,
+    ServiceInfoCollection
   },
   data() {
-    const times = []
-    for (let i = 1; i <= 24; i++) {
-      times.push(i + ':00')
-    }
+    // const times = []
+    // for (let i = 1; i <= 24; i++) {
+    //   times.push(i + ':00')
+    // }
     return {
-      times,
-      serviceActive: 'pickup',
-      pickup: {
-        date: '',
-        time: ''
-      }
+      // times,
+      // serviceActive: 'pickup',
+      // pickup: {
+      //   date: '',
+      //   time: ''
+      // }
     }
   },
   methods: {
-    activatePickup() {
-      this.serviceActive = 'pickup'
-    },
-    activateDelivery() {
-      this.serviceActive = 'delivery'
-    }
+    // activatePickup() {
+    //   this.serviceActive = 'pickup'
+    // },
+    // activateDelivery() {
+    //   this.serviceActive = 'delivery'
+    // }
   },
   mounted() {
     let fontAwesomeScript = document.createElement('script')
@@ -126,7 +108,7 @@ export default {
     padding: 0;
   }
 
-  nav {
+  .nav {
     display: flex;
     flex-direction: row;
     justify-content: space-between;
@@ -141,41 +123,6 @@ export default {
       color: #000;
       font-size: 50%;
       font-weight: 600;
-    }
-  }
-
-  .service-choice {
-    // height: 2rem;
-    color: #fff;
-    // width: 100%;
-    display: flex;
-    margin: 0.5rem 0.5rem 0 0.5rem;
-
-    .tab {
-      background-color: $secondary-color;
-      width: 50%;
-      text-align: center;
-      padding: 1rem 0 1rem 0;
-      border-radius: 20px 20px 0 0;
-    }
-
-    .active {
-      background-color: $primary-color;
-    }
-  }
-
-  .service-info {
-    height: 10rem;
-    background-color: $primary-color;
-    margin: 0 0.5rem 0.5rem 0.5rem;
-    border-radius: 0 0 20px 20px;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-
-    .display-none {
-      display: none;
     }
   }
 
