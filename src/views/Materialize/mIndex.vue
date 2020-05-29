2c45835cebbbf7adc9588020c95c7707d50fcb45
Xuzheng <linxz@garena.com>
2020-05-29 11:09:19 +0800

    ADD: playground for Materialize

---
+++ src/views/Materialize/mIndex.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>Materialize playground</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MaterializeHomepage'
+}
+</script>
+
+<style lang="scss" scoped>
+@import '@/styles/materialize-src/sass/materialize.scss';
+</style>
